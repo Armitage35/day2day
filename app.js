5be76e030395e2b2dbd3fd87824d59d5e572489f
armitage <adrien.dubois35@gmail.com>
2017-10-08 21:03:51 +0000

    initialize app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+var main = function() {
+    "use strict";
+    $(".comment-input button").on("click", function (event){
+        console.log("hello world");
+    });
+};
+
+$(document).ready(main);
