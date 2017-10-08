c3a8f9a41485602c1f8f360bb450a9b80fbe454c
armitage <adrien.dubois35@gmail.com>
2017-10-08 21:18:34 +0000

    add variable to store text

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var main = function() {
     "use strict";
     
     $(".comment-input button").on("click", function (event){
-        $(".comments").append("<p>this is a new comment</p>");
+        var $new_comment = $("<p>");
+        $new_comment.text("this is a new comment");
+        $(".comments").append($new_comment);
     });
 };
 
