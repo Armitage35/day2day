f5276b58237a30805864f8229fe05d43a273f508
armitage <adrien.dubois35@gmail.com>
2017-10-09 21:53:24 +0000

    enable hit enter to submit comment

--- app.js
+++ app.js
@@ -3,10 +3,31 @@ var main = function() {
     
     //adding new comments to the comment list
     $(".comment-input button").on("click", function (event){
-        //a new paragraph is being created AND THEN we add the content "this is a new comment"
-        var $new_comment = $("<p>").text($(".comment-input input").val());
-        $(".comments").append($new_comment);
+        var $new_comment;
+        
+        //making sure the box isn't empty (in order to avoid adding empty <p> as comments)
+        if ($(".comment-input input").val() !== "") {
+            //a new paragraph is being created AND THEN we add the content "this is a new comment"
+            $new_comment = $("<p>").text($(".comment-input input").val());
+            $(".comments").append($new_comment);
+            $(".comment-input input").val("")
+        }
+        
     });
+    
+    $(".comment-input input").on("keypress", function (event) {
+        
+        if (event.keyCode === 13){
+            var $new_comment;
+            //making sure the box isn't empty (in order to avoid adding empty <p> as comments)
+            if ($(".comment-input input").val() !== "") {
+                //a new paragraph is being created AND THEN we add the content "this is a new comment"
+                $new_comment = $("<p>").text($(".comment-input input").val());
+                $(".comments").append($new_comment);
+                $(".comment-input input").val("")
+            }
+        }
+    })
 };
 
 $(document).ready(main);
