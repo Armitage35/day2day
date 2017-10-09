6414872299996fe4a18109aee474d0eb3d5584f9
armitage <adrien.dubois35@gmail.com>
2017-10-09 22:03:17 +0000

    add font awesome

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var main = function() {
     "use strict";
     
-    //adding new comments to the comment list
+    //adding new comments to the comment list using the plus icon
     $(".comment-input button").on("click", function (event){
         var $new_comment;
         
@@ -15,8 +15,9 @@ var main = function() {
         
     });
     
+    //accepting events using the enter key
     $(".comment-input input").on("keypress", function (event) {
-        
+        //making sure the typed key is enter
         if (event.keyCode === 13){
             var $new_comment;
             //making sure the box isn't empty (in order to avoid adding empty <p> as comments)
