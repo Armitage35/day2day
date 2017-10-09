f56baa89282117418b1a774247a7b1583eec6602
armitage <adrien.dubois35@gmail.com>
2017-10-09 22:31:07 +0000

    branding

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
 var main = function() {
     "use strict";
     
-    //adding new comments to the comment list using the plus icon
-    $(".comment-input button").on("click", function (event){
+    //adding new tasks-list to the comment list using the PLUS ICON
+    $(".task-input button").on("click", function (event){
         var $new_comment;
         
-        //making sure the box isn't empty (in order to avoid adding empty <p> as comments)
-        if ($(".comment-input input").val() !== "") {
-            //a new paragraph is being created AND THEN we add the content "this is a new comment"
-            $new_comment = $("<p>").text($(".comment-input input").val());
-            $(".comments").append($new_comment);
-            $(".comment-input input").val("")
+        //making sure the box isn't empty (in order to avoid adding empty <p> as tasks-list)
+        if ($(".task-input input").val() !== "") {
+            //a new paragraph is being created AND THEN we add the content from the input field
+            $new_comment = $("<p>").text($(".task-input input").val());
+            console.log($new_comment);
+            $(".tasks-list").append($new_comment);
+            //empty input field
+            $(".task-input input").val("");
+            //fading the new comment in
+            $new_comment.hide();
+            $new_comment.fadeIn()
         }
         
     });
     
-    //accepting events using the enter key
-    $(".comment-input input").on("keypress", function (event) {
+    //accepting events using the ENTER KEY
+    $(".task-input input").on("keypress", function (event) {
         //making sure the typed key is enter
         if (event.keyCode === 13){
             var $new_comment;
-            //making sure the box isn't empty (in order to avoid adding empty <p> as comments)
-            if ($(".comment-input input").val() !== "") {
-                //a new paragraph is being created AND THEN we add the content "this is a new comment"
-                $new_comment = $("<p>").text($(".comment-input input").val());
-                $(".comments").append($new_comment);
-                $(".comment-input input").val("")
+            //making sure the box isn't empty (in order to avoid adding empty <p> as tasks-list)
+            if ($(".task-input input").val() !== "") {
+                //a new paragraph is being created AND THEN we add the content from the input field
+                $new_comment = $("<p>").text($(".task-input input").val());
+                $(".tasks-list").append($new_comment);
+                //empty input field
+                $(".task-input input").val("");
+                //fading the new comment in
+                $new_comment.hide();
+                $new_comment.fadeIn()
             }
         }
     })
