80c18db5807a5ce8c0260a333b7b93faf6b8c43b
armitage <adrien.dubois35@gmail.com>
2017-10-19 01:52:32 +0000

    refactor code

--- app.js
+++ app.js
@@ -3,27 +3,16 @@ var main = function() {
     //make tasks draggable
     Sortable.create(draggable, { /* options */ });
 
+    //hide tasks when completed
     $(".taskList").on('click', "input",function() {
         $(this).parent().fadeOut();
-        console.log("yolo");
-    }) 
+    });
     
-    //change when task is checked
-    /* $('input').change(function(){
-        if ($(this).is(':checked')) {
-            console.log("yolo");
-            $(this).parent().fadeOut();
-            }
-    }); */
-    
-    
-    
-    //adding new taskList to the task list using the PLUS ICON
-    $(".task-input button").on("click", function (event){
+    //adding tasks function
+    var addTaskFromInputBox = function () {
         var $new_task;
-        //making sure the box isn't empty (in order to avoid adding empty <p> as taskList)
         if ($(".task-input input").val() !== "") {
-            //a new paragraph is being created AND THEN we add the content from the input field
+           //a new paragraph is being created AND THEN we add the content from the input field
                 $new_task = $(".task-input input").val();
                 $(".taskList").append("<p class='task list-group-item'><i class='fa fa-bars' aria-hidden='true'></i> <input type='checkbox' name='task-marker'>" + $new_task + "</p>");
                 //empty input field
@@ -32,27 +21,20 @@ var main = function() {
                 $new_task.hide();
                 $new_task.fadeIn()
         }
-        
+    };
+    
+    //trigger task hiding on button click
+    $(".task-input button").on("click", function(event) {
+        addTaskFromInputBox();
     });
     
-    //accepting events using the ENTER KEY
-    $(".task-input input").on("keypress", function (event) {
-        //making sure the typed key is enter
-        if (event.keyCode === 13){
-            var $new_task;
-            //making sure the box isn't empty (in order to avoid adding empty <p> as taskList)
-            if ($(".task-input input").val() !== "") {
-                //a new paragraph is being created AND THEN we add the content from the input field
-                $new_task = $(".task-input input").val();
-                $(".taskList").append("<p class='task list-group-item'><i class='fa fa-bars' aria-hidden='true'></i><input type='checkbox' name='task-marker'>" + $new_task + "</p>");
-                //empty input field
-                $(".task-input input").val("");
-                //fading the new task in
-                $new_task.hide();
-                $new_task.fadeIn()
-            }
+    //trigger task hiding on enter key
+    $(".task-input input").on("keypress", function(event){
+        if (event.keyCode === 13) {
+            addTaskFromInputBox();
         }
-    });
+    })
+   
 };
 
 $(document).ready(main);
