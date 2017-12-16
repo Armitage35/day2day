c9ba91c4a8da9dee5be9ff53174ea776ec72d2c9
armitage <adrien.dubois35@gmail.com>
2017-12-16 23:11:30 +0000

    create server side JS file if it does not exists

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var express = require("express"),
 app.use(express.static(__dirname + "/client"));
 app.use(express.urlencoded());
 
-var port = process.env.PORT;
-var fs = require('fs');
+var port = process.env.PORT; //only for Cloud9
+var fs = require('fs'); //activate node file system
 http.createServer(app).listen(port);
 
 //this route takes the place of our todos.json in our former exemple
@@ -17,10 +17,9 @@ app.get("/todos.json", function(req, res) {
 app.post("/todos", function (req, res){
     console.log("data has been posted to the server");
     var userTask = req.body;
-    var yolo = JSON.stringify(userTask);
-    fs.writeFile('todos.json', yolo, 'utf8', function (err) {if (err) throw err; console.log('Saved!');});
-    //console.log(userTask);
-    console.log(yolo);
+    userTask = JSON.stringify(userTask);
+    fs.writeFile('todos.json', userTask, 'utf8', function (err) {if (err) throw err; console.log('Saved!');});
+    console.log(userTask);
    
    //send back a simple object
    res.json(userTask);
