b2329c3896f0db2a94148028b929b3c0fe389d75
armitage <adrien.dubois35@gmail.com>
2018-01-18 17:07:53 +0000

    quick css fixes

--- client/clientV2/appv2.js
+++ client/clientV2/appv2.js
@@ -82,7 +82,7 @@ var main = function() {
     //using the ctrl + enter keys to display the add task modal
     $(document).on("keypress", function(event) {
         if ((event.keyCode == 10 || event.keyCode == 13) && event.ctrlKey) {
-            $("#myInput").click();
+            $("#fixedBottom").click();
         }
     });
 
