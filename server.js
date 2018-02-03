368d3c9665a4a6d064fb62f7d91cfecf3fe311d5
armitage <adrien.dubois35@gmail.com>
2018-02-03 19:04:16 +0000

    barebone AWS integration

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ var express = require('express'),
     gravatar = require('gravatar'),
     compressor = require('node-minify'),
     upload = require('express-fileupload'),
+    AWS = require('aws-sdk'),
+    uuid = require('node-uuid'),
     app = express();
 
 app.use(express.static(__dirname + "/client"));
@@ -34,6 +36,26 @@ mongoose.connect('mongodb://localhost/day2day');
 var port = process.env.PORT || 80;
 http.createServer(app).listen(port);
 console.log("app working on port " + port)
+
+//loading AWS config
+AWS.config.loadFromPath('./awsCredentials.json');
+
+// Create an S3 client
+var s3 = new AWS.S3();
+
+// Create a bucket and upload something into it
+var bucketName = 'day2dayapp.net';
+var keyName = 'hello_world.txt';
+
+s3.createBucket({Bucket: bucketName}, function() {
+  var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
+  s3.putObject(params, function(err, data) {
+    if (err)
+      console.log(err)
+    else
+      console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+  });
+});
 
 //passort local strategy
 passport.use(new LocalStrategy(
