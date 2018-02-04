f750585912899825afea94d56eb8ee188ec82a49
armitage <adrien.dubois35@gmail.com>
2018-02-04 18:54:11 +0000

    create note model

---
+++ models/notes.js
@@ -0,0 +1,14 @@
+var mongoose = require("mongoose");
+
+//This is mongoose's model for todos
+var NoteSchema = mongoose.Schema({
+    noteBody: String,
+    createdOn: Date,
+    id: Number,
+    title: String,
+    userid: String,
+});
+
+var userNotes = mongoose.model("Task", NoteSchema);
+
+module.exports =  mongoose.model('Task', NoteSchema);
