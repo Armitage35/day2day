a692c15f4fac650314c6d76bd5fa6b773bf08449
armitage <adrien.dubois35@gmail.com>
2018-02-24 22:03:30 +0000

    use grunt to watch for changes in sass and pug files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,52 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		sass: {
-			dist: {
-				files: {
-					'client/style.css' : 'client/style.sass'
-				}
-			}
-		},
-		watch: {
-			css: {
-				files: 'client/*.sass',
-				tasks: ['sass']
-			}
-		}
-	});
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
-}
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    'client/style.css': 'client/style.sass'
+                }
+            }
+        },
+        pug: {
+            compile: {
+                options: {
+                    data: {
+                        debug: false,
+                        timestamp: '<%= new Date().getTime() %>'
+                    }
+                },
+                files: {
+                  'emailTemplates/htmlRender/welcomeEmail.html': 'emailTemplates/welcomeEmail.pug',
+                  'emailTemplates/htmlRender/passwordResetEmail.html': 'emailTemplates/passwordResetEmail.pug',
+                  'emailTemplates/htmlRender/passwordResetSuccess.html': 'emailTemplates/passwordResetSuccess.pug',
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'client/*.sass',
+                tasks: ['sass']
+            },
+            pug: {
+                files: 'emailTemplates/*.pug',
+                tasks: ['pug']
+            }
+        }
+        // Uglify does not support ES6 for now...
+        // uglify: {
+        //     options: {
+        //         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        //     },
+        //     build: {
+        //         src: 'client/app.js',
+        //         dest: 'client/app.min.js'
+        //     }
+        // }
+    });
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-pug');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['watch']);
+};
