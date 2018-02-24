a6056737c55c89e9649885f99b8d66e412327fd4
armitage <adrien.dubois35@gmail.com>
2018-02-24 21:36:08 +0000

    use gunt to compile sass

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'client/style.css' : 'client/style.sass'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: 'client/*.sass',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['watch']);
+}
