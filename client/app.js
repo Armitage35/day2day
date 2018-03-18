d3fb62dc1baf461d92403c9a23fbb46a0fee6ae4
armitage <adrien.dubois35@gmail.com>
2018-03-18 17:26:26 +0000

    fist ugly version of calendar integration

--- client/app.js
+++ client/app.js
@@ -1138,3 +1138,77 @@ initializeDay2Day();
 if (Cookies.get('userid') !== undefined && window.location.pathname === "./auth.html") {
     window.location = "index.html";
 }
+
+/**
+       *  Called when the signed in status changes, to update the UI
+       *  appropriately. After a sign-in, the API is called.
+       */
+      function updateSigninStatus(isSignedIn) {
+         if (isSignedIn) {
+            authorizeButton.style.display = 'none';
+            signoutButton.style.display = 'block';
+            listUpcomingEvents();
+         }
+         else {
+            authorizeButton.style.display = 'block';
+            signoutButton.style.display = 'none';
+         }
+      }
+
+      /**
+       *  Sign in the user upon button click.
+       */
+      function handleAuthClick(event) {
+         gapi.auth2.getAuthInstance().signIn();
+      }
+
+      /**
+       *  Sign out the user upon button click.
+       */
+      function handleSignoutClick(event) {
+         gapi.auth2.getAuthInstance().signOut();
+      }
+
+      /**
+       * Append a pre element to the body containing the given message
+       * as its text node. Used to display the results of the API call.
+       *
+       * @param {string} message Text to be placed in pre element.
+       */
+      function appendPre(message) {
+         var pre = document.getElementById('content');
+         var textContent = document.createTextNode(message + '\n');
+         pre.appendChild(textContent);
+      }
+
+      /**
+       * Print the summary and start datetime/date of the next ten events in
+       * the authorized user's calendar. If no events are found an
+       * appropriate message is printed.
+       */
+      function listUpcomingEvents() {
+         gapi.client.calendar.events.list({
+            'calendarId': 'primary',
+            'timeMin': (new Date()).toISOString(),
+            'showDeleted': false,
+            'singleEvents': true,
+            'maxResults': 10,
+            'orderBy': 'startTime'
+         }).then(function(response) {
+            var events = response.result.items;
+            console.log(events);
+
+            if (events.length > 0) {
+               for (i = 0; i < events.length; i++) {
+                  var event = events[i];
+                  var when = event.start.dateTime;
+                  if (!when) {
+                     when = event.start.date;
+                  }
+               }
+            }
+            else {
+               appendPre('No upcoming events found.');
+            }
+         });
+      }
